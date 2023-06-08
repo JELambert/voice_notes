3139042b32315b83ebea3b184166b778d10891a9
Josh <joshua.lambert@knights.ucf.edu>
2023-06-07 22:02:22 -0500

    update openai

--- Home.py
+++ Home.py
@@ -3,21 +3,14 @@ from langchain.llms import OpenAI
 import whisper
 import os
 from tempfile import NamedTemporaryFile
+import openai
 
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 st.title('🎤🗒️ VoiceMarkdown!')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
-
-model = whisper.load_model("base.en")
-
-def run_whisper(input_file, output_path):
-    result = model.transcribe(input_file)
-    with open(output_path, 'w') as f:
-        f.write(result['text'])
-        f.close()
-    return result['text']
+openai.api_key = openai_api_key
 
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
@@ -26,12 +19,8 @@ def generate_response(input_text):
 audio_file = st.file_uploader('Upload your audio file:', type='m4a')
 if audio_file:
     with st.spinner("Transcribing..."):
-        with NamedTemporaryFile(suffix="m4a") as temp:
-            temp.write(audio_file.getvalue())
-            temp.seek(0)
-            model = whisper.load_model("base")
-            result = model.transcribe(temp.name)
-            st.write(result["text"])
+        result = openai.Audio.transcribe("whisper-1", audio_file)
+        st.write(result["text"])
 
 
 st.markdown('\n')
